91411bcae27881c0c563c398d2aa3bef6dec09df
Emmanuel Alabi <emmanuelalabi@Emmanuels-MacBook-Pro-2.local>
2019-08-05 14:25:49 +0100

    Updates

---
+++ precache-manifest.915ce3f1b20338ef1fd1aadab74bfcdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfc0966c105cc8ca75652611aa08d8d3",
+    "url": "/easyhomes/index.html"
+  },
+  {
+    "revision": "0553b3e81c2dde0cd4d3",
+    "url": "/easyhomes/static/css/main.a72b0fd3.chunk.css"
+  },
+  {
+    "revision": "48c89fe0f2c493513d38",
+    "url": "/easyhomes/static/js/2.768fa400.chunk.js"
+  },
+  {
+    "revision": "0553b3e81c2dde0cd4d3",
+    "url": "/easyhomes/static/js/main.03870dce.chunk.js"
+  },
+  {
+    "revision": "c64b52d86c986392c2b5",
+    "url": "/easyhomes/static/js/runtime~main.d9159861.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/easyhomes/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/easyhomes/precache-manifest.915ce3f1b20338ef1fd1aadab74bfcdd.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/easyhomes/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
